504cda900e30782467e78765659fadd2cb05e324
Saglara <nohaevass@yandex.ru>
2023-01-26 21:52:08 +0300

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -1,5 +1,15 @@
+// Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом 
+// (возвращает boolean значение).
 
+import java.util.Scanner;
 
 public class Ex3 {
-    
+    public static void main(String[] args) {
+        Scanner scString = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = scString.nextLine();
+        scString.close();
+        
+    }
+
 }
