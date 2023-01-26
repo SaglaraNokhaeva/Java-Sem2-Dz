3ffc93d4459080a9e09a298ff8c0dbd23aafc4f1
Saglara <nohaevass@yandex.ru>
2023-01-26 19:17:03 +0300

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,20 +1,38 @@
-// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+
+// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой 
+//итерации запишите в лог-файл.
+import java.sql.Array;
 import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Введите разменость массива: ");
-        int razmernost =input.nextInt();
-        int array []=new int[razmernost];
+        int razmernost = input.nextInt();
+        int arr[] = new int[razmernost];
         System.out.println("Введите элемент массива: ");
         for (int i = 0; i < razmernost; i++) {
-            array[i] = input.nextInt(); 
+            arr[i] = input.nextInt();
         }
-        System.out.print ("Исходный массив: ");
+        System.out.print("Исходный массив: ");
         for (int i = 0; i < razmernost; i++) {
-            System.out.print (" " + array[i]); 
+            System.out.print(" " + arr[i]);
         }
         System.out.println();
+        // сортировка
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1-i; j++) {
+                if (arr[j] >= arr[j + 1]) {
+                    int p = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = p;
+                }
+            }
+        }
+        System.out.print("Отсортированный массив: ");
+        for (int i = 0; i < razmernost; i++) {
+            System.out.print(" " + arr[i]);
+        }
+
     }
 }
